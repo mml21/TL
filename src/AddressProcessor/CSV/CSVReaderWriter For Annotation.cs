cbfe8bfd4cfc48ba1b24f2b1a417751908924052
Marcos Mainar <marcosmainar@gmail.com>
2017-06-11 11:54:34 +0100

    Added comments for code review exercise

--- src/AddressProcessor/CSV/CSVReaderWriter For Annotation.cs
+++ src/AddressProcessor/CSV/CSVReaderWriter For Annotation.cs
@@ -8,9 +8,22 @@ namespace AddressProcessing.CSV
 
         Please leave the rest of this file as it is so we can discuss your concerns during the next stage of the interview process.
         
-        *)
-        *)
-        *)
+        1) This class does too much, breaking the Single Responsability Principle (SRP). 
+           This is immediately obvious just from the name used for this class. 
+           It would be cleaner and more maintainable to split this class into two CSVReader and CSVWriter classes.
+        2) This class should implement the IDisposable interface to clean up the file stream/s (_readerStream and _writerStream).
+           This will enable it to be used within a Using statement, preventing the users of it from forgetting to call the Close method.
+        3) There are two Read public methods which look almost identical (breaks DRY, too much code duplication), 
+           but crucially one of them assigns values to input parameters, which won't be visible to the caller.
+           In addition this method does no validation on the string returned from the ReadLine private method.
+        4) Write methods uses expensive string concatenation (since strings are inmutable a new copy will be created each time 
+           that the string concatenation operator + is used). Would be better to use StringBuffer.
+        5) Some public methods are missing input/arguments validation for some arguments (e.g. Open, Write). 
+           For instance in the Open method, the fileName argument could be null or not corresponding to an existing
+           file for reading. This will still throw an exception from the .NET System.IO methods but we could do argument validation ourselves beforehand.
+           Also in the Write method, the input columns array could be null.
+        6) Separator character is defined and used in various methods, could be made a field of this class.
+           Also the outPut variable in Write method has inconsistent casing on its name.
     */
 
     public class CSVReaderWriterForAnnotation
